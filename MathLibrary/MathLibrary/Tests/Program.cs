d20cae29e8b8bae6789d2f755a1d64319282fc15
tulrich <tulrich@rhrk.uni-kl.de>
2021-02-18 16:47:27 +0100

    Added MathLibrary projects

---
+++ MathLibrary/MathLibrary/Tests/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using MathLib;
+
+
+namespace Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
