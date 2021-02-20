e96edeafab8202f2b5286a39a329454d5a1d9de1
tulrich <tulrich@rhrk.uni-kl.de>
2021-02-20 22:20:35 +0100

    Added Transpose() and Determinant() to MyMatrix and tests for these functions

--- MathLibrary/Tests/MyMatrixTests.cs
+++ MathLibrary/Tests/MyMatrixTests.cs
@@ -12,6 +12,8 @@ namespace Tests
             testAddition();
             testSubtraction();
             testMultiplication();
+            testTranspose();
+            testDeterminant();
         }
 
         private static void testAddition()
@@ -61,6 +63,37 @@ namespace Tests
             MyMatrix result_2 = matrix_20 * matrix_21;
             if (result_2 != new MyMatrix(new MyFraction[,] { { 10, 13, 13 }, { 22, 29, 31 } }))
                 Console.Out.WriteLine("[MyMatrixTests/testAddition] Test 2 failed.");
+        }
+        private static void testTranspose()
+        {
+            MyMatrix expectedMatrix_0 = new MyMatrix(new MyFraction[,] { { 1 } });
+            if ((new MyMatrix(new MyFraction[,] { { 1 } })).Transpose() != expectedMatrix_0)
+                Console.Out.WriteLine("[MyMatrixTests/testTranspose] Test 0 failed.");
+
+            MyMatrix expectedMatrix_1 = new MyMatrix(new MyFraction[,] { { 1, 3 }, { 2, 4 } });
+            if ((new MyMatrix(new MyFraction[,] { { 1, 2 }, { 3, 4 } })).Transpose() != expectedMatrix_1)
+                Console.Out.WriteLine("[MyMatrixTests/testTranspose] Test 1 failed.");
+
+            MyMatrix expectedMatrix_2 = new MyMatrix(new MyFraction[,] { { 1, 4 }, { 2, 5 }, { 3, 6 } });
+            if ((new MyMatrix(new MyFraction[,] { { 1, 2, 3 }, { 4, 5, 6 } })).Transpose() != expectedMatrix_2)
+                Console.Out.WriteLine("[MyMatrixTests/testTranspose] Test 2 failed.");
+        }
+        private static void testDeterminant()
+        {
+            if ((new MyMatrix(new MyFraction[,] { { 1 } })).Determinant() != 1)
+                Console.Out.WriteLine("[MyMatrixTests/testDeterminant] Test 0 failed.");
+
+            if ((new MyMatrix(new MyFraction[,] { { 1, 2 }, { 3, 4 } })).Determinant() != -2)
+                Console.Out.WriteLine("[MyMatrixTests/testDeterminant] Test 1 failed.");
+
+            if ((new MyMatrix(new MyFraction[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } })).Determinant() != 0)
+                Console.Out.WriteLine("[MyMatrixTests/testDeterminant] Test 2 failed.");
+
+            if ((new MyMatrix(new MyFraction[,] { { 1, 1, 2, 3 }, { 2, 3, 1, 2 }, { 1, 1, 3, 1 }, { 3, 1, 1, 1 } })).Determinant() != -38)
+                Console.Out.WriteLine("[MyMatrixTests/testDeterminant] Test 3 failed.");
+
+            if ((new MyMatrix(new MyFraction[,] { { 1, 1, 2, 3, 4 }, { 2, 3, 1, 2, 4 }, { 1, 1, 3, 1, 4 }, { 3, 1, 1, 1, 4 }, { 3, 3, 2, 1, 3 } })).Determinant() != 86)
+                Console.Out.WriteLine("[MyMatrixTests/testDeterminant] Test 4 failed.");
         }
     }
 }
